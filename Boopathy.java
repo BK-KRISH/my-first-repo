0816d7e8d1e9683efe1b60645aed07e7e2243b25
BK-KRISH <baranikrishnan007@gmail.com>
2025-04-21 16:18:20 +0530

    Add files via upload

---
+++ Boopathy.java
@@ -0,0 +1,19 @@
+public class Boopathy
+{
+int a,b,c;
+void add();
+{
+a=10;
+b=20;
+c=a+b;
+System.out.println("Result=" +c)
+}
+public static void main(String arg[])
+{
+Boopathy obj=new Boopathy();
+obj.add();
+}
+}
+
+
+
